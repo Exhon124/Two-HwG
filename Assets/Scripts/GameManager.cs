024be52dd221d91cdf0e41b05af537bb134e152e
Matthew Hernandez <120426849+Exhon124@users.noreply.github.com>
2025-02-24 23:21:23 -0500

    Pipe (Prebonk)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,16 +8,35 @@ public class GameManager : MonoBehaviour
     public bool keyCard = false;
     public bool macGuffin = false;
     public Color endCanvasColor = Color.clear;
+    public bool hasPipe = false;
+    private Transform player;
+    public GameObject pipePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player")?.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (hasPipe == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Throw");
+                hasPipe = false;
+                Vector3 spawnPosition = player.position + player.transform.forward * 4;
+
+                GameObject pipe = Instantiate(pipePrefab, spawnPosition, player.transform.rotation);
+
+                Rigidbody rb = pipe.GetComponent<Rigidbody>();
+                rb.velocity = player.transform.forward * 10;
+
+
+            }
+        }
     }
+
 }
